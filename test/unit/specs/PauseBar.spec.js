8c73f3e1bef29d3cf5771186896b0e1c67e8fff5
Tobias Beuving <informatiebureau@ehhh.nl>
2017-09-26 18:17:51 +0200

    Added  polyfill for Object.assign

--- test/unit/specs/PauseBar.spec.js
+++ test/unit/specs/PauseBar.spec.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import store from '@/store/store'
 import PauseBar from '@/components/PauseBar'
+// @todo check if storing the state is still necessary
+if (typeof Object.assign !== 'function') {
+  Object.assign = function (target) {
+    'use strict'
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object')
+    }
+
+    target = Object(target)
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index]
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key]
+          }
+        }
+      }
+    }
+    return target
+  }
+}
+
 function deepClone (obj) {
   let cloned = Object.assign({}, obj) //  new {a:1, b:2, c:3};
   return cloned
 }
+
 const initialState = deepClone(store.state)
 
 describe('PauseBar.vue (welcome state)', () => {
